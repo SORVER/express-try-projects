f00c57b839df3030b12825a7e1a71b3b96895924
SORVER <boodymostaf24@gmail.com>
2025-02-18 02:20:06 +0200

    Task Manager Almost Done

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,25 +1,69 @@
-const { get } = require("../routes/tasks");
-const Task = require('../models/task')
-const getAllTasks = (request, response) => {
-    response.json(request.body)
-}
+const Task = require('../models/task');
+
+const getAllTasks = async (request, response) => {
+    try {
+        const tasks = await Task.find({});
+        response.status(200).json({ tasks });
+    } catch (error) {
+        response.status(500).json({ msg: error });
+    }
+};
 
 const createTask = async (request, response) => {
-    const task = await Task.create(request.body);
-    response.status(201).json({task});
-}
-const deleteTask = (request, response) => {
-    response.send('hello from Tasks');
-}
-const updateTask = (request, response) => {
-    response.send('hello from Tasks');
-}
+    try {
+        const task = await Task.create(request.body);
+        response.status(201).json({ task });
+    } catch (error) {
+        response.status(500).json({ msg: error });
+    }
+};
 
-const getTask = (request, response) => {
-    response.send('hello from Tasks');
-}
+const deleteTask = async (request, response) => {
+    try {
+        const task = await Task.findByIdAndDelete(request.params.id);
+        if (!task) {
+            return response.status(404).json({ msg: `No task with id: ${request.params.id}` });
+        }
+        response.status(200).json({ task });
+    } catch (error) {
+        response.status(500).json({ msg: error });
+    }
+};
 
+const updateTask = async (request, response) => {
+    try {
+        const task = await Task.findByIdAndUpdate(
+            request.params.id,
+            
+            request.body,
+            { new: true, runValidators: true }
+        );
+        if (!task) {
+            return response.status(404).json({ msg: `No task with id: ${request.params.id}` });
+        }
+        response.status(200).json({ task });
+    } catch (error) {
+        response.status(500).json({ msg: error });
+    }
+};
+
+const getTask = async (request, response) => {
+    try {
+        const task = await Task.findById(request.params.id);
+
+        if (!task) {
+            return response.status(404).json({ msg: `No task with id: ${request.params.id}` });
+        }
+        response.status(200).json({ task });
+    } catch (error) {
+        response.status(500).json({ msg: error });
+    }
+};
 
 module.exports = {
-    getAllTasks, getTask,createTask,updateTask,deleteTask
-}
+    getAllTasks,
+    getTask,
+    createTask,
+    updateTask,
+    deleteTask
+};
